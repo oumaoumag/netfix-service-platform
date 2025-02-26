6fe7c0bc282561b2c01c3b983f328c1853e260d3
sjuma <sheillajuma001@gmail.com>
2025-02-26 18:31:04 +0300

    most requested service tho show on home page...

--- main/views.py
+++ main/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render, redirect
+from django.db.models import Count
+
 from django.contrib.auth import logout as auth_logout
+from services.models import Service, RequestService
+
 
 
 def home(request):
-    return render(request, "main/home.html", {})
+    most_requested_services = Service.objects.annotate(
+        request_count=Count('requestservice')
+    ).order_by('-request_count')[:3]  # Get top 5 most requested services
+    print(most_requested_services)
+    return render(request, 'main/home.html', {
+        'most_requested_services': most_requested_services
+    })
+    #return render(request, "main/home.html", {})
 
 
 def logout(request):
